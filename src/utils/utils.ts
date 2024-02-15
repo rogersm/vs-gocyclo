353540b04a6dc2cd3125771200eecdd1f9d9ad85
Roger Sen <roger.sen@gmail.com>
2024-02-15 20:01:22 +0100

    Added getActiveWorkspacePath() to support newer vscode releases.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,3 +7,8 @@ export function getActiveFilePath(): string {
         return "";
     }
 }
+
+export function getActiveWorkspacePath(): string | undefined {
+  const fileName = vscode.window.activeTextEditor?.document.fileName;
+  return vscode.workspace.workspaceFolders?.map((folder) => folder.uri.fsPath).filter((fsPath) => fileName?.startsWith(fsPath))[0];
+}
